71fc01fdc30d422a85cb3efb4f830a960c785c7b
soyfully <soyfully@gmail.com>
2021-01-19 22:41:04 +0900

    work page 수정

--- src/views/work/work.js
+++ src/views/work/work.js
@@ -2,87 +2,35 @@ import $ from 'jquery'
 import Swiper from 'swiper/js/swiper.esm.bundle'
 
 export default {
-    methods: {
-
+    data: function () {
+        return {
+            workJson: this.$jsonLabel.work,
+        }
     },
     mounted () {
-        $('html, body').attr('_init', 'true');
-
-        worksEvent().init();
+        this.changeDefaultBackColor();
+        this.setSwiper();
     },
-}
+    methods: {
+        changeDefaultBackColor() {
+            $('html, body').css('background-color', 'lightsteelblue');
+        },
+        setSwiper() {
+            setTimeout(function () {
+                new Swiper ('.swiper-container', {
+                    observer: true,
+                    observeParents: true,
+                    observeSlideChildren: true,
+                    mousewheel: true,
+                    grabCursor: true,
+                    freeMode: true,
+                    freeModeSticky: true,
+                    slidesPerView: 'auto',
+                })
+            }, 0)
+        },
+        openTargetModal() {
 
-var worksEvent = function () {
-    function init () {
-        $('html, body').css('background-color','lightsteelblue');
-
-        bindEvent();
-
-        // set swiper
-        setTimeout(function () {
-            new Swiper ('.swiper-container', {
-                observer: true,
-                observeParents: true,
-                observeSlideChildren: true,
-                mousewheel: true,
-                grabCursor: true,
-                freeMode: true,
-                freeModeSticky: true,
-                slidesPerView: 'auto',
-            })
-        }, 0)
-    }
-
-    function bindEvent () {
-        var thisIndex;
-
-        $('.list-wrap .list').on('click', function () {
-            thisIndex = $(this).index();
-
-            open(thisIndex);
-
-            return thisIndex;
-        })
-
-        $('.popup-close-icon').on('click', function () {
-            close(thisIndex);
-        })
-    }
-
-    function open (i) {
-        if (!$('.popup-content').eq(i)[0]) return false;
-
-        $('html, body').css('overflow','hidden').attr('_state','open');
-
-        $('.sub, .main').removeClass('reverse').addClass('move');
-
-        setTimeout(function () {
-            $('.popup-component').eq(i).addClass('open');
-        }, 300)
-        setTimeout(function () {
-            $('.popup-content').eq(i).removeClass('close').addClass('open').css('display', 'block');
-        }, 600)
-    }
-
-    function close (i) {
-        $('html, body').css('overflow','visible').attr('_state','close');
-
-        $('.popup-content').eq(i).removeClass('open').addClass('close');
-        setTimeout(function () {
-            $('.sub, .main').removeClass('move').addClass('reverse');
-        }, 200)
-        setTimeout(function () {
-            $('.popup-component').eq(i).removeClass('open');
-        }, 600)
-
-        $('.popup-content').on("animationend", function(){
-            if ($('html, body').attr('_state') == 'close') {
-                $('.popup-content').eq(i).css('display','none');
-            }
-        });
-    }
-
-    return {
-        init : init
-    }
+        }
+    },
 }
--- src/views/work/work.vue
+++ src/views/work/work.vue
@@ -6,12 +6,12 @@
         <div class="container">
             <div class="wrap">
                 <div class='page-explain'>
-                    <div class='title'>Works</div>
+                    <div class='title'>{{ workJson.title }}</div>
                     <div class='sub-title'>
-                        회사를 다니며 진행한 프로젝트 중 몇 가지를 골라 정리했습니다.<br>
-                        그 외, 스터디 및 회사에서 수월한 프로젝트 진행을 위해<br>
-                        만들어놓은 개인 작업물도 있습니다.<br>
-                        더 많은 프로젝트는 <a href='https://www.rocketpunch.com/@soyfully' target="_blank">여기</a>를 클릭해 주세요.
+                        {{ workJson.subTitle.desc1 }} <br>
+                        {{ workJson.subTitle.desc2 }} <br>
+                        {{ workJson.subTitle.desc3 }} <br>
+                        {{ workJson.subTitle.desc4 }} <a v-bind:href="workJson.subTitle.link" target='_blank'>{{ workJson.subTitle.linkText }}</a>{{ workJson.subTitle.desc5 }}
                     </div>
                 </div>
                 <div class='page-content'>
@@ -19,7 +19,6 @@
                         <div class="sub-title">
                             <div class="sub-title-name">In the company</div>
                             <div class="sub-title-scroll">
-                                <!-- <div class="text">scroll</div> -->
                                 <div class="icon">
                                     <em style=""></em>
                                     <em style=""></em>
@@ -28,48 +27,16 @@
                         </div>
                         <div class="list-wrap swiper-container">
                             <div class="wrap swiper-wrapper">
-                                <div class="list swiper-slide">
+                                <div class="list swiper-slide" v-for="( list, index ) in workJson.workList" :key="index">
                                     <div class="list-title">
-                                        대림 이편한 세상 사이트 리뉴얼
-                                        <span>메인 프로젝트</span>
+                                        {{ list.title }}
+                                        <span>{{ list.subTitle }}</span>
                                     </div>
                                     <div class="list-content">
-                                        <div>Gulp</div>
-                                        <div>Pug / Scss</div>
+                                        <div v-for="( skill, index ) in list.skill" :key="index">{{ skill }} </div>
                                     </div>
+                                    <a v-if="list.link" v-bind:href="list.link"></a>
                                 </div>
-                                <div class="list swiper-slide">
-                                    <div class="list-title">
-                                        동아제약 OTC 통합 브랜드 사이트 구축
-                                        <span>메인 프로젝트</span>
-                                    </div>
-                                    <div class="list-content">
-                                        <div>반응형</div>
-                                        <div>Gulp</div>
-                                        <div>Pug / Scss</div>
-                                    </div>
-                                </div>
-                                <a class="list swiper-slide" href="https://github.com/soyfully/loading-library" target="_blank">
-                                    <div class="list-title">
-                                        Loading Icon UI 컴포넌트
-                                        <span>서브 프로젝트</span>
-                                    </div>
-                                    <div class="list-content">
-                                        <div>es6</div>
-                                        <div>Typescript</div>
-                                        <div>Webpack</div>
-                                    </div>
-                                </a>
-                                <a class="list swiper-slide" href="https://github.com/soyfully/make-component" target="_blank">
-                                    <div class="list-title">
-                                        Common UI 컴포넌트
-                                        <span>서브 프로젝트</span>
-                                    </div>
-                                    <div class="list-content">
-                                        <div>Vanilla JS</div>
-                                        <div>Webpack</div>
-                                    </div>
-                                </a>
                             </div>
                         </div>
                     </div>
